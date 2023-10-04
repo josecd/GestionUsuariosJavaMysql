139eeced545f79acb0db2faaf1e8a60f6b28df93
Jose Alejandro <44279311+josecd@users.noreply.github.com>
2023-10-04 18:32:43 -0500

    Initial commit

---
+++ src/main/java/com/mycompany/gestionusuarios/GestionUsuarios.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+package com.mycompany.gestionusuarios;
+
+import com.mycompany.gestionusuarios.forms.Formulario;
+
+/**
+ *
+ * @author jose-
+ */
+public class GestionUsuarios {
+
+    public static void main(String[] args) {
+        Formulario ventana = new Formulario();
+        ventana.show();
+    }
+}
---
+++ src/main/java/com/mycompany/gestionusuarios/models/Cliente.java
@@ -0,0 +1,55 @@
+package com.mycompany.gestionusuarios.models;
+
+public class Cliente {
+
+    private String id;
+    private String email;
+    private String apellido;
+    private String nombre;
+    private String telefono;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getNombreComplero() {
+        return nombre + " " + apellido;
+    }
+}
